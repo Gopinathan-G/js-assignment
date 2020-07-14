5d5e57002863e79b0b8cd17f4e8791719ac23871
Gopinathan G <68248460+Gopinathan-G@users.noreply.github.com>
2020-07-15 04:29:39 +0530

    Javascript Day 1 Assignment
    LetsUpgrade JS Day 1 Assignment Submitted

---
+++ intro.js
@@ -0,0 +1,23 @@
+// console.log() method 
+console.log('hey i am gopi')
+console.log('BCA');  
+console.log(5); 
+// console.error() method
+console.error('there is error in this program')
+// console.warn() method
+console.warn('this is a warning') 
+// console.table() method 
+console.table({'a':5, 'b':8}); 
+// console.count() method 
+for(let i=5;i<9;i++)
+{ 
+    console.count(i); 
+}; 
+// console.group() and console.groupEnd() method 
+console.group('start'); 
+console.warn('warning!'); 
+console.error('error here'); 
+console.log('javascript'); 
+console.groupEnd('close'); 
+console.log('new section'); 
+
